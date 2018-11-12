02b8117bcd82301a2cbbf8163a07dcc0f51ed9ee
Grande <44588596+Veloxme@users.noreply.github.com>
2018-11-12 08:48:48 -0700

    empleados

--- application/models/Empleados_modelo.php
+++ application/models/Empleados_modelo.php
@@ -1,15 +1,15 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Empresa_modelo extends CI_Model{
+class Empleados_modelo extends CI_Model{
 	
 	public function __construct(){
         parent::__construct();
 	}
-	public function obtenerEmpresa($estatus) {
+	public function obtenerEmpleados($estatus) {
 		if($estatus == 1 || $estatus == 0) {
 			$this->db->where('estatus', $estatus);
 		} 
-		$query = $this->db->get('empresas');
+		$query = $this->db->get('empleados');
 		if($query->num_rows() > 0) {
 			return $query->result();
 		}
@@ -26,8 +26,7 @@ class Empresa_modelo extends CI_Model{
 
 	public function obtenerDatos($id) {
 		$this->db->where('id', $id);
-		$query = $this->db->get('empresas')->row();
-        $query->descripcion =  str_replace('-',',', $query->descripcion);
+		$query = $this->db->get('empleados')->row();
 		return $query;
 	}
 
@@ -40,14 +39,14 @@ class Empresa_modelo extends CI_Model{
 		}
 		$estado = array('estatus' => $estatus);
 		$this->db->where('id', $id);
-		$this->db->update('empresas', $estado);
+		$this->db->update('empleados', $estado);
 	}
 	
-	public function agregarEmpresa($data, $nombreArchivo) {
+	public function agregarEmpleados($data, $nombreArchivo) {
 		$this->db->where('nombre', $data['inpNombreE']);
-		$query = $this->db->get('empresas');
+		$query = $this->db->get('empleados');
 		if($query->num_rows() > 0) {
-			return array('exito' => false, 'msg' => '<li>El nombre de la empresa ya se encuentra registrado</li>');
+			return array('exito' => false, 'msg' => '<li>El nombre del empleado ya se encuentra registrado</li>');
 		}
 		$this->db->where('nombre', $data['inpNombre']);
 		$aux = $this->db->get('Usuarios');
@@ -59,47 +58,45 @@ class Empresa_modelo extends CI_Model{
 				"nombre" => $data['inpNombre'],
 				"contraseña" => password_hash($data['inpContrasena'], PASSWORD_DEFAULT),
 				"correo" => $data['inpCorreo'],
-				"idperfil" => 2,
+				"idperfil" => 5,
 				"estatus" => 1
 			);
 			$this->db->insert('Usuarios', $datosUsuario);
 		}
 		$this->db->where('nombre', $data['inpNombre']);
 		$query = $this->db->get('usuarios')->row();
-		$datosEmpresa = array(
+		$datosEmpleados = array(
 			'nombre' => $data['inpNombreE'],
-			'razonsocial' => $data['inpRazonSocial'],
-            'descripcion' => str_replace(',','-',$data['inpDescripcion']),
-			'rfc' => $data['inpRFC'],
+			'apellidomaterno' => $data['inpMaterno'],
+            'apellidopaterno' => $data['inpPaterno'],
 			'domicilio' => $data['inpDomicilio'],
 			'telefono' => $data['inpTelefono'],
-			'local' => $data['inpLocal'],
 			'estatus' => 1,
 			'id_usuario' => $query->id
 		);
-		$this->db->insert('empresas', $datosEmpresa);
+		$this->db->insert('empleados', $datosEmpleados);
         $idRegistrado = $this->db->insert_id();
 			if($this->db->affected_rows() > 0) {
-                $imagenEmpresa = array("logotipo" => 'assets/Empresas/'.$idRegistrado.'/'.$nombreArchivo);
+                $imagenEmpleados = array("imagen" => 'assets/Empleados/'.$idRegistrado.'/'.$nombreArchivo);
                 $this->db->where('id', $idRegistrado);
-                $this->db->update('empresas', $imagenEmpresa);
+                $this->db->update('empleados', $imagenEmpleados);
 				return array('exito' => true, 'msg' => $idRegistrado);
 			}
 			else {
-				return array('exito' => false, 'msg' => '<li>No se guardo la empresa en la base de datos, intente de nuevo</li>');
+				return array('exito' => false, 'msg' => '<li>No se guardo el empleado en la base de datos, intente de nuevo</li>');
 			}
 	}
 
-	public function modificarEmpresa($data, $nombreArchivo = '') {
-        $this->db->select('empresas.logotipo');
+	public function modificarEmpleados($data, $nombreArchivo = '') {
+        $this->db->select('empleados.imagen');
         $this->db->where('id', $data['id']);
-        $nombreArchivop = $this->db->get('empresas')->row();
-		if($data['cambio'] == 0 && $data['inpNombreE'] == $data['oldNombre'] && $data['inpDescripcion'] == $data['oldDescripcion'] && $data['inpRazonSocial'] == $data['oldRazonSocial'] && $data['inpRFC'] == $data['oldRFC'] && $data['inpDomicilio'] == $data['oldDomicilio'] && $data['inpTelefono'] == $data['oldTelefono'] && $data['inpLocal'] == $data['oldLocal']) {
+        $nombreArchivop = $this->db->get('empleados')->row();
+		if($data['cambio'] == 0 && $data['inpNombreE'] == $data['oldNombre'] && $data['inpMaterno'] == $data['oldMaterno'] && $data['inpPaterno'] == $data['oldPaterno'] && $data['inpDomicilio'] == $data['oldDomicilio'] && $data['inpTelefono'] == $data['oldTelefono']) {
 			return array('exito' => false, 'msg' =>'No se actualizaron los datos porque no hubo cambios');
 		}
 		if($data['inpNombreE'] != $data['oldNombre']) {
 			$this->db->where('nombre', $data['inpNombreE']);
-			$query = $this->db->get('empresas');
+			$query = $this->db->get('empleados');
 			if($query->num_rows() > 0) {
 				$query = $query->row();
 				if($query->nombre === $data['inpNombreE']) {
@@ -109,29 +106,25 @@ class Empresa_modelo extends CI_Model{
 			if($data['cambio'] != 0) {
 				$datos = array(
 					'nombre' => $data['inpNombreE'],
-					'descripcion' => str_replace(',','-',$data['inpDescripcion']),
-					'razonsocial' => $data['inpRazonSocial'],
-                    'rfc' => $data['inpRFC'],
+                    'apellidomaterno' => $data['inpMaterno'],
+                    'apellidopaterno' => $data['inpPaterno'],
                     'domicilio' => $data['inpDomicilio'],
                     'telefono' => $data['inpTelefono'],
-                    'local' => $data['inpLocal'],
-					'logotipo' => 'assets/Empresas/'.$data['id'].'/'.$nombreArchivo
+					'imagen' => 'assets/Empleados/'.$data['id'].'/'.$nombreArchivo
 				);
-                unlink($nombreArchivop->logotipo);
+                unlink($nombreArchivop->imagen);
 			}
 			else {
 				$datos = array(
 					'nombre' => $data['inpNombreE'],
-					'descripcion' => str_replace(',','-',$data['inpDescripcion']),
-					'razonsocial' => $data['inpRazonSocial'],
-                    'rfc' => $data['inpRFC'],
+                    'apellidomaterno' => $data['inpMaterno'],
+                    'apellidopaterno' => $data['inpPaterno'],
                     'domicilio' => $data['inpDomicilio'],
-                    'telefono' => $data['inpTelefono'],
-                    'local' => $data['inpLocal']
+                    'telefono' => $data['inpTelefono']
 				);
 			}
 			$this->db->where('id', $data['id']);
-			$this->db->update('empresas', $datos);
+			$this->db->update('empleados', $datos);
 			if($this->db->affected_rows() > 0) {
 				return array('exito' => true, 'msg' => '');
 			}
@@ -139,31 +132,27 @@ class Empresa_modelo extends CI_Model{
 				return array('exito' => false, 'msg' => 'No se actualizo la base de datos');
 			}
 		}
-		else if($data['cambio'] != 0 || $data['inpDescripcion'] != $data['oldDescripcion'] || $data['inpRazonSocial'] != $data['oldRazonSocial'] || $data['inpRFC'] != $data['oldRFC'] || $data['inpDomicilio'] != $data['oldDomicilio'] || $data['inpTelefono'] != $data['oldTelefono'] || $data['inpLocal'] != $data['oldLocal']) {
+		else if($data['cambio'] != 0 || $data['inpMaterno'] != $data['oldMaterno'] || $data['inpPaterno'] != $data['oldPaterno'] || $data['inpDomicilio'] != $data['oldDomicilio'] || $data['inpTelefono'] != $data['oldTelefono']) {
 			if($data['cambio'] != 0) {
 				$datos = array(
-					'descripcion' => str_replace(',','-',$data['inpDescripcion']),
-					'razonsocial' => $data['inpRazonSocial'],
-                    'rfc' => $data['inpRFC'],
+                    'apellidomaterno' => $data['inpMaterno'],
+                    'apellidopaterno' => $data['inpPaterno'],
                     'domicilio' => $data['inpDomicilio'],
                     'telefono' => $data['inpTelefono'],
-                    'local' => $data['inpLocal'],
-					'logotipo' => 'assets/Empresas/'.$data['id'].'/'.$nombreArchivo
+					'imagen' => 'assets/Empleados/'.$data['id'].'/'.$nombreArchivo
 				);
-                unlink($nombreArchivop->logotipo);
+                unlink($nombreArchivop->imagen);
 			}
 			else {
 				$datos = array(
-					'descripcion' => str_replace(',','-',$data['inpDescripcion']),
-					'razonsocial' => $data['inpRazonSocial'],
-                    'rfc' => $data['inpRFC'],
+				    'apellidomaterno' => $data['inpMaterno'],
+                    'apellidopaterno' => $data['inpPaterno'],
                     'domicilio' => $data['inpDomicilio'],
-                    'telefono' => $data['inpTelefono'],
-                    'local' => $data['inpLocal']
+                    'telefono' => $data['inpTelefono']
 				);
 			}
 			$this->db->where('id', $data['id']);
-			$this->db->update('empresas', $datos);
+			$this->db->update('empleados', $datos);
 			if($this->db->affected_rows() > 0) {
 				return array('exito' => true, 'msg' => '');
 			}
