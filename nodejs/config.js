947f4dccc2d9912b2cd04e84715ff091768b4988
Adrian Scott <2016030032@upsin.edu.mx>
2019-01-08 13:41:06 -0700

    Corrección de confuración en la bd

--- nodejs/config.js
+++ nodejs/config.js
@@ -1,16 +1,16 @@
-/* dbConfig = {
+dbConfig = {
     host: "Pespeciales.upsin.edu.mx",
     user: "pt1v3n2d0s",
     password: "L4ti9o5s",
     database: "venadospruebas"
-} */
+}
 
-dbConfig = {
+/* dbConfig = {
     host: "localhost",
     user: "root",
     password: "",
     database: "venadospruebas"
-}
+} */
 
 module.exports = {
     'jwtSecretKey': 'ADQWDWLD6DW46D5QD$!#ASDADSDA989789561231879879',
