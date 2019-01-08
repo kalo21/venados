0e0424e706adc4b63cd3ff3b7fe5c17a2a7a6271
Adrian Scott <2016030032@upsin.edu.mx>
2019-01-08 13:30:09 -0700

    Server actualizado

--- nodejs/db.js
+++ nodejs/db.js
@@ -1,24 +1,41 @@
 var mysql = require('mysql');
 var config = require('./config.js');
-var con = mysql.createConnection(config.dbConfig);
+var con;
+/**
+ * Para conecar y recuperar la conexión si se cae.
+ */
+function handleDisconnect() {
+    con = mysql.createConnection(config.dbConfig); // Recreate the connection, since
+                                                    // the old one cannot be reused.
+  
+    con.connect(function(err) {              // The server is either down
+      if(err) {                                     // or restarting (takes a while sometimes).
+        console.log('error when connecting to db:', err);
+        setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+      }                                     // to avoid a hot loop, and to allow our node script to
+      console.log('Conectado')
+    });                                     // process asynchronous requests in the meantime.
+                                            // If you're also serving http, display a 503 error.
+    con.on('error', function(err) {
+      console.log('db error', err);
+      if(err.code === 'PROTOCOL_CONNECTION_LOST' || err.code === 'ECONNRESET') { // Connection to the MySQL server is usually
+        handleDisconnect();                         // lost due to either server restart, or a
+      } else {                                      // connnection idle timeout (the wait_timeout
+        throw err;                                  // server variable configures this)
+      }
+    });
+  }
+  
+  handleDisconnect();
 
-con.connect(function(err) {
-    if (err) console.log("Error al conectar con la base de datos");
-    else{
-        console.log("Connected!");
-    }
-
-});
 function insertPedido(data, callback){
     con.beginTransaction(function(err) {
         if (err) { 
-            con.end();   
             return callback(false) 
         }
         var sql = `INSERT INTO pedidos (idempresa, idusuario, total, estatus) VALUES ('${data.idEmpresa}', '${data.idUsuario}', '${data.total}', 'Solicitado')`;
         con.query(sql, function (err, result) {
         if(err) {
-            con.end();   
             return callback(false);
         }
         var lastId = result.insertId;
@@ -34,7 +51,7 @@ function insertPedido(data, callback){
         }
         con.query(dtlSql, [detalles], function (err, result) {
             if(err) {
-                con.end();      
+
                 return callback(false);
             }
             var sqlSaldo = `UPDATE clientes, usuarios SET saldo = saldo - ${data.total} WHERE id_usuario = ${data.idUsuario} AND    clientes.id_usuario = usuarios.id`;
@@ -55,7 +72,7 @@ function insertPedido(data, callback){
                             }
                         })
                     }
-                    con.end();   
+    
                 });
             });
         });
@@ -64,13 +81,12 @@ function insertPedido(data, callback){
 function cancelarPedido(data, callback){
     con.beginTransaction(function(err) {
         if(err){
-            con.end();   
             return callback(false);
         }
         var sql = `UPDATE pedidos SET estatus = 'Cancelado' WHERE pedidos.id = ${data.idPedido} AND estatus = 'Solicitado'`;
         con.query(sql, function (err, result) {
             if(err){
-                con.end();   
+
                 return callback(false);
             }
             var sql2 = `UPDATE clientes SET saldo = saldo + ${data.total} WHERE id = ${data.usuario}`;
@@ -89,7 +105,7 @@ function cancelarPedido(data, callback){
                         }
                     })
                 }
-                con.end(); 
+
             });     
         });
     });
